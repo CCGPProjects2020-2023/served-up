d9f71e842edd862d4bb6c1547ebe688bcc85d29a
Ikamjot Hundal <ihundal@my.centennialcollege.ca>
2023-03-26 17:33:38 -0400

    mistake in commenting

--- Assets/[Scripts]/PlayerController.cs
+++ Assets/[Scripts]/PlayerController.cs
@@ -139,7 +139,7 @@ public class PlayerController : MonoBehaviour
             /*if (hitObject.TryGetComponent(out ContainerCounter container))
             {
                 container.GetItem();
-            } /*
+            } */
         }
     }
 
