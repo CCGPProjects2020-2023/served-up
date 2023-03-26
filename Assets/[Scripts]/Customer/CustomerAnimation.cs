68f29aa82ce36e00f2e99dc661391ce25e85bbd5
Grant <grant.macmillan94@gmail.com>
2023-03-26 17:10:04 -0400

    Walking Anim for customer

---
+++ Assets/[Scripts]/Customer/CustomerAnimation.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CustomerAnimation : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = this.gameObject.GetComponent<NavMeshAgent>();
+        anim = this.gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (agent.velocity.magnitude > 0.05)
+        {
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+    }
+}
